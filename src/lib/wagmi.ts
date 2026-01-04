ea6ea8c6d8211effa6f1a66132bb0df8ec345e62
RainyXY <shellkxy@126.com>
2026-01-04 23:44:45 +0800

    完善

--- src/lib/wagmi.ts
+++ src/lib/wagmi.ts
@@ -2,8 +2,8 @@ import { getDefaultConfig } from '@rainbow-me/rainbowkit';
 import { sepolia } from 'wagmi/chains';
 
 export const config = getDefaultConfig({
-	appName: 'My Blog',
-	projectId: 'YOUR_WALLETCONNECT_PROJECT_ID',
+	appName: 'my_blog',
+	projectId: '066569847d951e6fe2625eb6ab6e967e',
 	chains: [sepolia],
 	ssr: true,
 });
